8a0fabd9fe530453587444efe8ee3499cce753b2
lethanh01 <lethanh24092002@gmail.com>
2025-11-25 22:44:32 +0700

    update prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,28 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    // Tạo pg client
+    const client = new pg.Client({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    // Tạo adapter
+    const adapter = new PrismaPg(client);
+
+    // Gọi PrismaClient với adapter
+    super({
+      adapter,
+    });
+
+    // Kết nối pg client (bắt buộc)
+    client.connect();
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
